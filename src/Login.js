75af48a375b7f343fb6b65e8a0bb2f71e2a7c05b
ramieeee <rambotty@gmail.com>
2022-04-08 12:35:16 +0900

    integrate loginbutton

--- src/Login.js
+++ src/Login.js
@@ -1,20 +1,43 @@
 import { useState } from "react";
-import LoginButton from "./LoginButton";
+import axios from "axios";
 
 function Login() {
     const [userId, setUserId] = useState("");
     const [userPw, setUserPw] = useState("");
+    const [tocken, setTocken] = useState("");
 
     function eventChange(event) {
         setUserId(event.target.value);
         setUserPw(event.target.value);
     }
 
+    const handleClick = () => {
+        axios.post("/login", {
+                "username": userId,
+                "password": userPw
+        })
+        .then(function(response) {
+            // when successful
+            setTocken(response.headers.authorization)
+            console.log(tocken);
+            
+            axios.get("/api/card/list", tocken)
+            .then(function(response) {
+                console.log(response);
+            }).catch(function(error) {
+                console.log('**ERROR**:' + error.response.data);
+            });
+
+        }).catch(function(error) {
+            console.log('**ERROR:' + error.response + '**');
+        });
+    };
+
     return(
         <form className='loginForm'>
             <input type="text" onChange={eventChange}  name="id" className='inputField' placeholder="ID" required="required"/>
             <input type="password" onChange={eventChange}name="pw" className='inputField' placeholder="PW" required="required" autoComplete="off"/>
-            <LoginButton valueId={userId} valuePw={userPw}/>
+            <input type="button" className="submitButton" value="로그인" onClick={handleClick}/>
         </form>
     )
 }
