6ffac41226cc80153bf1703416df46b488cc5371
ramieeee <rambotty@gmail.com>
2022-04-08 12:00:49 +0900

    Add function for user ID, PW input to useState vars

--- src/Login.js
+++ src/Login.js
@@ -1,16 +1,20 @@
-// import { useState } from "react";
-// import { useHistory } from "react-router-dom";
-// import Axios from "axios";
-
+import { useState } from "react";
 import LoginButton from "./LoginButton";
 
 function Login() {
+    const [userId, setUserId] = useState("");
+    const [userPw, setUserPw] = useState("");
+
+    function eventChange(event) {
+        setUserId(event.target.value);
+        setUserPw(event.target.value);
+    }
 
     return(
         <form className='loginForm'>
-            <input type="text" name="userId" className='inputField' placeholder="아이디"/>
-            <input type="password" name="password" className='inputField' placeholder="비밀번호"/>
-            <LoginButton/>
+            <input type="text" onChange={eventChange}  name="id" className='inputField' placeholder="ID" required="required"/>
+            <input type="password" onChange={eventChange}name="pw" className='inputField' placeholder="PW" required="required" autoComplete="off"/>
+            <LoginButton valueId={userId} valuePw={userPw}/>
         </form>
     )
 }
--- src/LoginButton.js
+++ src/LoginButton.js
@@ -1,17 +1,28 @@
 import axios from "axios";
+import { useState } from "react";
 
+function LoginButton({ valueId="", valuePw="" }) {
+    const [tocken, setTocken] = useState("");
 
-
-function LoginButton() {
     const handleClick = () => {
         axios.post("/login", {
-                "username": "test",
-                "password": "1234"
+                "username": valueId,
+                "password": valuePw
         })
         .then(function(response) {
-            console.log(response);
+            // when successful
+            setTocken(response.headers.authorization)
+            console.log(tocken);
+
+            // axios.get("/user/list", tocken)
+            // .then(function(response) {
+            //     console.log(response);
+            // }).catch(function(error) {
+            //     console.log('**ERROR**:' + error.response.data);
+            // });
+
         }).catch(function(error) {
-            console.log('**ERROR**:' + error.response.data);
+            console.log('**ERROR:' + error.response + '**');
         });
     };
 
